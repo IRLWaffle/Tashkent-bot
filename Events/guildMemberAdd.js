a2b8b89cf6adeb5530295f008c2a33e47a26fbe8
Waffle <andreiguma9@gmail.com>
2021-12-04 11:32:56 +0200

    forgot this shit

--- Events/guildMemberAdd.js
+++ Events/guildMemberAdd.js
@@ -3,10 +3,10 @@ const Event = require("../Structures/Event");
 module.exports = new Event("guildMemberAdd", async (client, member) => {
   const userId = member.id;
   const welcomeMessage = `Welcome to the Emporium <@${userId}>, hope you like it here dear comrade.`;
-  // const roleToAdd = member.guild.roles.cache.get("591811564319932416");
+  const roleToAdd = member.guild.roles.cache.get("591811564319932416");
   const welcomeChannel = member.guild.channels.cache.find(
     (name) => name.name === "welcome"
   );
-  // await member.roles.add(roleToAdd);
+  await member.roles.add(roleToAdd);
   welcomeChannel.send({ content: welcomeMessage });
 });
