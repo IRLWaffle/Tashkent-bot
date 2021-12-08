27244dde6a916993a290f8ede5900e70ab702877
Waffle <andreiguma9@gmail.com>
2021-12-08 20:59:07 +0200

    fixed again

--- Events/dadJoke.js
+++ Events/dadJoke.js
@@ -9,7 +9,7 @@ module.exports = new Event("messageCreate", (client, message) => {
     message.content.toLowerCase() === "i'm"
   )
     return;
-  if (words[0] === "im" || words[0] === `i'm`) {
+  if (words[0].toLowerCase() === "im" || words[0].toLowerCase() === `i'm`) {
     //gets the message, splits it into an array without any spaces, removes the first item and joins them together
     const messageDad = message.content.split(/ +/).splice(1).join(" ");
     const randomIm =
