1ea7baaddb7b0a803f6c2fe24fe71f51a18230ee
Waffle <andreiguma9@gmail.com>
2021-12-08 20:57:01 +0200

    dad joke fixed

--- Events/dadJoke.js
+++ Events/dadJoke.js
@@ -2,16 +2,14 @@ const Event = require("../Structures/Event");
 
 module.exports = new Event("messageCreate", (client, message) => {
   const PartReplies = ["I am", "I'm"];
+  const words = message.content.split(/ +/);
   if (
     message.author.bot ||
     message.content.toLowerCase() === "im" ||
     message.content.toLowerCase() === "i'm"
   )
     return;
-  if (
-    message.content.toLowerCase().startsWith("im") ||
-    message.content.toLowerCase().startsWith("i'm")
-  ) {
+  if (words[0] === "im" || words[0] === `i'm`) {
     //gets the message, splits it into an array without any spaces, removes the first item and joins them together
     const messageDad = message.content.split(/ +/).splice(1).join(" ");
     const randomIm =
