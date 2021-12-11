7d382953d7c5570976be4856ebbb96fca0920ff8
Waffle <andreiguma9@gmail.com>
2021-12-11 17:47:08 +0200

    lol again

--- Events/messageCreate.js
+++ Events/messageCreate.js
@@ -1,12 +1,17 @@
 const Event = require("../Structures/Event");
 const dotenv = require("dotenv");
 dotenv.config();
-const prefix = "T!" || "t!";
+const prefix1 = "T!";
+const prefix2 = "t!";
 
 module.exports = new Event("messageCreate", (client, message) => {
-  if (!message.content.startsWith(prefix)) return;
+  if (
+    !message.content.startsWith(prefix1) ||
+    !message.content.startsWith(prefix2)
+  )
+    return;
 
-  const args = message.content.substring(prefix.length).split(/ +/);
+  const args = message.content.substring(prefix1.length).split(/ +/);
 
   const command = client.commands.find((cmd) => cmd.name === args[0]);
   if (!command) return message.channel.send("Not a valid command");
