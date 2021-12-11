cc38686f3f71ac8b502568f91a076ccb4043c7fc
Waffle <andreiguma9@gmail.com>
2021-12-11 17:42:53 +0200

    prefix change

--- Events/messageCreate.js
+++ Events/messageCreate.js
@@ -1,11 +1,12 @@
 const Event = require("../Structures/Event");
 const dotenv = require("dotenv");
 dotenv.config();
+const prefix = "T!" || "t!";
 
 module.exports = new Event("messageCreate", (client, message) => {
   if (!message.content.startsWith(process.env.PREFIX)) return;
 
-  const args = message.content.substring(process.env.PREFIX.length).split(/ +/);
+  const args = message.content.substring(prefix.length).split(/ +/);
 
   const command = client.commands.find((cmd) => cmd.name === args[0]);
   if (!command) return message.channel.send("Not a valid command");
