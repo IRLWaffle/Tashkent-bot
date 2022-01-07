2c1778d0f09a1a0be7a83bfe783e1693caf220a1
Waffle <andreiguma9@gmail.com>
2022-01-07 12:43:22 +0200

    oof

--- commands/Others/activePlayers.js
+++ commands/Others/activePlayers.js
@@ -1,23 +1,35 @@
 const Command = require("../../Structures/Command");
 const MemberSchema = require("../../Schemas/memberSchema");
 const Discord = require("discord.js");
+const mongo = require("../../mongo");
 
 module.exports = new Command({
   name: "activePlayers",
   description: "the players who have participated in the event",
   run: async (message, args, client) => {
     const embed = new Discord.MessageEmbed()
-      .setTitle("**The people that participated in the merry kissmas event")
+      .setTitle("**The people that participated in the merry kissmas event**")
       .setColor("ORANGE");
-    const members = await MemberSchema.memberPumpkin.find({
-      guildId: message.guild.id,
+
+    let members;
+
+    await mongo().then(async (mongoose) => {
+      try {
+        members = await MemberSchema.memberPumpkin.find({
+          guildId: message.guild.id,
+        });
+      } catch (err) {
+        console.log(err);
+      }
     });
+
     const userId = [];
     for (let i = 0; i < members.length; i++) {
       userId.push(members[i].userId);
     }
+    console.log(userId);
     for (let i = 0; i < userId.length; i++) {
-      let user = await message.guild.members.fetch(idArray[i]);
+      let user = await message.guild.members.fetch(userId[i]);
       embed.addField(`${user.user.username}`);
     }
     message.channel.send({ embeds: [embed] });
