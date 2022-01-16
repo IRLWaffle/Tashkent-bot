abad5120eb7f81874c8a92ee3b49dffd22d7b5ff
Waffle <andreiguma9@gmail.com>
2022-01-17 00:05:39 +0200

    member remove fix

--- Events/guildMemberRemove.js
+++ Events/guildMemberRemove.js
@@ -1,13 +1,10 @@
 const Event = require("../Structures/Event");
-const { removeDoc } = require("../Structures/DatabaseFunctions");
 
-module.exports = new Event("guildMemberRemove", async (client, member) => {
+module.exports = new Event("guildMemberRemove", (client, member) => {
+  console.log("a");
   const leaveMessage = `Oh, comrade ${member.user.username} has left. I see...shame, more vodka for the rest of us then.`;
-  const guildId = member.guild.id;
-  const userId = member.id;
   const leaveChannel = member.guild.channels.cache.find(
     (name) => name.name === "sacrifices"
   );
-  await removeDoc(guildId, userId);
   leaveChannel.send({ content: leaveMessage });
 });
